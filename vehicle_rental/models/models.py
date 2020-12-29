d9d86a88f5e0360118016735c55b589c60995835
sonu <sonusomankp@gmail.com>
2020-12-29 17:34:12 +0530

    [ADD]form and kanbanmodified

--- vehicle_rental/models/models.py
+++ vehicle_rental/models/models.py
@@ -6,13 +6,14 @@ from odoo import models, fields, api
 class vehicle_rental(models.Model):
     _name = 'vehicle_rental.vehicle_rental'
     _description = 'vehicle_rental.vehicle_rental'
+    _rec_name = 'v_name'
 
     vehicle = fields.Many2one('fleet.vehicle', string="Vehicle")
-    v_name = fields.Char(string='Name')
+    v_name = fields.Char(string='Name', required=True)
     brand = fields.Many2one('fleet.vehicle.model.brand', string="Brand")
     registration = fields.Date(string="Registration Date")
     model = fields.Char(string='Model')
-    rent = fields.Float(string='Rent')
+    rent = fields.Monetary(string='Rent')
     currency_id = fields.Many2one('res.currency', string='Currency',
                                   default=lambda self: self.env.user.company_id.currency_id)
     state = fields.Selection([('type1', 'Available'), ('type2', 'Not Available'), ('type3', 'Sold')], 'Type',
@@ -23,12 +24,3 @@ class fleet_inherit(models.Model):
     _inherit = 'fleet.vehicle'
 
     registration_date = fields.Date('Registration Date ', required=False)
-
-    #   value = fields.Integer()
-    #   value2 = fields.Float(compute="_value_pc", store=True)
-
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
