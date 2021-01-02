d0a674ff934527b561c98f845bd1346ac4a5866d
sonu <sonusomankp@gmail.com>
2021-01-02 17:18:54 +0530

    [ADD]Vehicle 2.0 Updated form,Date validation completed,Vehicle name generated

--- vehicle_rental/models/models.py
+++ vehicle_rental/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class VehicleRental(models.Model):
     _name = 'vehicle.rental'
@@ -8,7 +9,7 @@ class VehicleRental(models.Model):
 
     vehicle_id = fields.Many2one('fleet.vehicle', string="Vehicle",
                                  domain=[('state_id', '=', 3)])
-    vehicle_name = fields.Char(string='Name', required=True)
+    vehicle_name = fields.Char(string='Name')
     brand_id = fields.Many2one(string='Brand', related='vehicle_id.brand_id',
                                readonly=True, store=True)
     registration = fields.Date(string='Registration Date',
@@ -22,6 +23,7 @@ class VehicleRental(models.Model):
     state = fields.Selection(
         [('Available', 'Available'), ('Not Available', 'Not Available'),
          ('Sold', 'Sold')], 'Status', default='Available')
+
     _sql_constraints = [
         ('vehicle_name', 'unique (vehicle_name)',
          'Vehicle name already exists!!')]
@@ -31,6 +33,11 @@ class VehicleRental(models.Model):
         for rec in self:
             rec.model=rec.registration
 
+    @api.onchange('vehicle_id')
+    def _onchange_vehicle_name(self):
+        for rec in self:
+            rec.vehicle_name = rec.vehicle_id.name
+
 
 class RentRequest(models.Model):
     _name = 'rent.request'
@@ -39,17 +46,18 @@ class RentRequest(models.Model):
                        copy=False, default='New')
     customer_id = fields.Many2one('res.partner',string="Customer Name")
     request_date = fields.Date('Request Date', default=fields.Date.today)
-    vehicle_id = fields.Many2one('fleet.vehicle',string="Vehicle")
+    vehicle_id = fields.Many2one('vehicle.rental',string="Vehicle")
     from_date = fields.Date('From date')
     to_date = fields.Date('To date')
     period = fields.Char(string='Period')
     currency_id = fields.Many2one('res.currency', string='Currency',
                                   default=lambda
                                       self: self.env.user.company_id.currency_id)
-    rent_request = fields.Monetary(string='Rent',reference="vehicle_id.rent")
+    rent_request = fields.Monetary(string='Rent', related="vehicle_id.rent")
     state = fields.Selection(
         [('Draft', 'Draft'), ('Confirm', 'Confirm'),
          ('Returned', 'Returned')], 'State', default='Draft')
+
 
     @api.model
     def create(self, vals):
@@ -59,6 +67,11 @@ class RentRequest(models.Model):
         result = super(RentRequest, self).create(vals)
         return result
 
+    @api.constrains('to_date','from_date')
+    def date_check(self):
+        for rec in self:
+            if rec.to_date < rec.from_date:
+                raise ValidationError(('Sorry Date Invalid'))
 
 
 class FleetInherit(models.Model):
