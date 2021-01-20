b1ecce422357b5da6741710c5e536e64fd58d50c
sonu <sonusomankp@gmail.com>
2021-01-20 17:40:45 +0530

    [ADD]Vehicle Rent PDF Report- Query added, Table created

--- vehicle_rental/reports/rent_report.py
+++ vehicle_rental/reports/rent_report.py
@@ -4,8 +4,7 @@ from odoo import models, fields, api
 
 
 class RentalReport(models.Model):
-
-    _name = 'rental.report'
+    _name = 'report.vehicle_rental.rental_report'
 
     vehicle_id = fields.Many2one('rent.request', 'Vehicle')
     from_date = fields.Date('From Date')
@@ -15,16 +14,24 @@ class RentalReport(models.Model):
         data = {
             'model_id': self.id,
         }
-        return self.env.ref('rent.request.rental_report').report_action(self, data=data)
+        return self.env.ref('vehicle_rental.print_report_pdf').report_action(
+            self,
+            data=data)
 
     @api.model
     def _get_report_values(self, docids, data):
         model_id = data['model_id']
+        print(model_id)
         value = []
-        query = """ SELECT *FROM rent_request """
+        query = """SELECT*
+        FROM rent_request as rent
+        INNER JOIN res_partner as customer
+        ON rent.customer_id = customer.id"""
+
         value.append(model_id)
         self._cr.execute(query, value)
         record = self._cr.dictfetchall()
+        print(record)
         return {
             'docs': record,
             'date_today': fields.Datetime.now(),
